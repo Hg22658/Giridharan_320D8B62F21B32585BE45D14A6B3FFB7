3b442c9e6d42e9f294a6a3afb1188555133689aa
Hg22658 <144784342+Hg22658@users.noreply.github.com>
2023-10-11 12:05:31 +0530

    Add files via upload

--- challenge 3.1.py
+++ challenge 3.1.py
@@ -1,24 +1,14 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
+def linearSearchProduct(productList, targetProduct):
+    indices = []
 
-def sort_students(students):
-    return sorted(students, key=lambda student: student.cgpa, reverse=True)
+    for index, product in enumerate(productList):
+        if product == targetProduct:
+            indices.append(index)
 
+    return indices
 
-students = [
-    Student('EREN YEAGER', '001', 8.5),
-    Student('ZORO', '007', 6.7),
-    Student('GOJO SATORU', '002', 8.6),
-    Student('MADARA UCHIHA', '005', 9.9),
-    Student('SAITAMA', '006', 9.6),
-    Student('GOKU', '003', 9.7),
-    Student('LIGHT YAGAMI', '004', 5.7),
-]
-
-sorted_students = sort_students(students)
-
-for student in sorted_students:
-  print("Name: {}, Roll Number: {}, Cgpa: {}".format(student.name, student.roll_number, student.cgpa))
+#Example usage:
+products = ["Audi", "Bugatti", "Skyline", "Bugatti", "BMW", "Bugatti", "Rolls_Royce", "Supra", "Bugatti"]
+target = "Bugatti"
+result = linearSearchProduct(products, target)
+print(result)
